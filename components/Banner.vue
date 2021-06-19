deadeec0c26864c5af803bf358ed6521683c67b2
Andrew Westwick <awestwick@gmail.com>
2021-06-19 13:51:21 -0500

    initial commit

---
+++ components/Banner.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="banner">
+    <div class="container mx-auto">
+      <div class="link-back">
+        <a href="#">‹ Visit Sonoma Clean Power</a>
+      </div>
+      <div class="banner-right">
+        <div class="banner-contact">1 (866) 878-6008 | Contact Us</div>
+        <div class="banner-social">
+          <i class="fab fa-facebook-square"></i>
+          <i class="fab fa-twitter-square"></i>
+          <i class="fab fa-instagram-square"></i>
+          <i class="fab fa-youtube-square"></i>
+          <i class="fab fa-linkedin"></i>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+#banner {
+  width: 100%;
+  height: 48px;
+  background: #382658;
+  color: #fff;
+  display: flex;
+  align-items: center;
+}
+#banner .container {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.banner-right {
+  display: flex;
+  align-items: center;
+}
+.banner-contact {
+  padding-right: 96px;
+}
+.banner-social {
+  font-size: 24px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,145 @@
+<template>
+  <div>
+    <div>
+      <Banner />
+      <div class="hero-home">
+        <div class="container mx-auto">
+          <div class="grid grid-cols-2">
+            <div class="cta">
+              <h1>Rebates for home energy improvements</h1>
+              <p>
+                A brighter future starts at home. Earn rebates for
+                energy-efficient solutions that help you save money and live
+                comfortably.
+              </p>
+              <a href="#" class="button-cta">Get Started</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="home-info">
+        <div class="container mx-auto">
+          <div class="grid grid-cols-2">
+            <div class="info-1">
+              <h2>Live your best life</h2>
+              <p>
+                Sonoma Clean Power offers rebates and resources that make it
+                easier to choose energy-saving products for your home. From
+                appliances to heating and cooling systems, we’re here to help
+                you live cleaner and greener. Apply today to receive customer or
+                contractor rebates for qualified products and services.
+              </p>
+            </div>
+            <div class="info-2"><h2>Applying is easy</h2></div>
+          </div>
+        </div>
+      </div>
+      <div class="get-started">
+        <p>GET STARTED</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.hero-home {
+  /* height: 680px; */
+  background: url('~/assets/images/home.jpg') no-repeat center center;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  padding-top: 96px;
+  padding-bottom: 160px;
+}
+.cta {
+  background: rgba(56, 38, 88, 0.9);
+  color: #fff;
+  padding: 56px;
+  margin-right: 40px;
+}
+.cta p {
+  font-size: 20px;
+  margin: 24px 0;
+}
+h1 {
+  font-size: 40px;
+  font-weight: bold;
+  line-height: 1.2;
+}
+h2 {
+  font-size: 36px;
+  font-weight: bold;
+}
+.home-info {
+  margin-top: -64px;
+}
+.info-1,
+.info-2 {
+  padding: 64px;
+}
+.info-1 {
+  background: #fff;
+}
+.info-1 h2 {
+  color: #382658;
+}
+.info-1 p {
+  font-size: 18px;
+}
+.info-2 {
+  background: #382658;
+  color: #fff;
+}
+.get-started p {
+  color: #8d969a;
+  font-size: 16px;
+  font-weight: bold;
+}
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+/* .container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+} */
+</style>
